8637dda77f6e232c4f8f41c965ce8ab13a75431e
oggunderscore <oggunderscore@oggundecoresMBP.lan>
2022-07-09 14:28:30 -0700

    [fix] Fixed login and token issues

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import discord, teslapy, json
 
+# TODO: KNOWN BUGS LIST
+# Add bugs here
+
 with open('tokens.json', 'r') as openfile:
     tokens = json.load(openfile)
 
+# TODO: Remove Teslatoken.json as it is not needed anymore, remove references to it as well
 with open('teslatoken.json', 'r') as openfile:
     tokendictionary = json.load(openfile)
 
@@ -43,15 +47,39 @@ class MyClient(discord.Client):
                 emailDictionary = json.load(openfile)
             if str(client.user.id) not in emailDictionary:
                 await message.channel.send('Email was not found in directory, Please set it using: t! email [email]')
+
+            # TODO: The below code can be removed
             #check if token is placed
             with open('teslatoken.json', 'r') as secondfile:
                 tokendictionary = json.load(secondfile)
-            if str(client.user.id) not in tokendictionary:
-                await message.channel.send('Token was not found in directory, Please set it using: t! token [token]')
+                
+            # TODO: Removed token check because it was causing issues
+            #if str(client.user.id) not in tokendictionary:
+            #    await message.channel.send('Token was not found in directory, Please set it using: t! token [token]')
+            #print("\n\nEmail: " + emailDictionary[str(client.user.id)] + "\tToken: " + tokendictionary[str(client.user.id)] + "\n\n")
+            
+            # TODO: Shortcut for login, should be globalized? Is this even possible in a safe manner
+            tesla = teslapy.Tesla(emailDictionary[str(client.user.id)])
+            
+            if not tesla.authorized:
 
-            if not teslapy.Tesla(emailDictionary[str(client.user.id)]).authorized:
-                teslapy.Tesla(emailDictionary[str(client.user.id)]).fetch_token(authorization_response=tokendictionary[str(client.user.id)])
-                if teslapy.Tesla(emailDictionary[str(client.user.id)]).authorized:
+                # Prompt user to open the URL in a browser and enter the code
+                await message.channel.send("Use browser to login. Page Not Found will be shown at success.\nOpen this URL: " + tesla.authorization_url())
+                
+                # Wait for their reply with the url
+                msg = await client.wait_for('message', check=None)
+                # TODO: Implement a check to see if the message is the url using "check" function
+                # Refer to: https://discordpy.readthedocs.io/en/latest/api.html#discord.Message
+
+                # Debugging
+                # print("\n\nToken: " + msg.content + "\n\n")
+
+                # Use the code to fetch the access token
+                tesla.fetch_token(authorization_response=msg.content)
+
+                # TODO: Old code
+                #tesla.fetch_token(authorization_response=tokendictionary[str(client.user.id)])
+                if tesla.authorized:
                     await message.channel.send('Successful login.')
                 else:
                     await message.channel.send('Something went wrong, maybe incorrect email or token type?')
@@ -84,6 +112,8 @@ class MyClient(discord.Client):
                 outfile.write(json_object)
             await message.channel.send('Email has been set')
 
+
+        # TODO: This function can be removed entirely, t! token is not needed with a proper working t! login
         if message.content.startswith('t! token'):
             text = message.content
             token = text.split()[-1]
@@ -118,4 +148,5 @@ class MyClient(discord.Client):
 
 client = MyClient()
 client.run(tokens['discordToken'])
-tesla.close()
+# TODO: Integrate where on bot close, it will terminate the teslapy session (but this is a per user session?)
+# tesla.close() 
